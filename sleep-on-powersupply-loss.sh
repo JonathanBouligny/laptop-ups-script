89880068d947973b100789a00a165d8597c01b9c
root <root@farnsworth.wehaveaproblem.net>
2024-05-24 12:47:33 -0500

    first commit

---
+++ sleep-on-powersupply-loss.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+while true; do
+    # Check if the laptop is plugged in
+    power_status=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -E "state|to full" | awk '{print $2}')
+
+    if [ "$power_status" = "discharging" ]; then
+        # Put the laptop to sleep
+        systemctl suspend --no-block
+
+        # Wait for a short duration to allow the suspend process to finish
+        sleep 2
+
+        # Exit the loop to prevent multiple sleep triggers
+        break
+    fi
+
+    # Wait for 5 seconds before checking again
+    sleep 5
+done
+
